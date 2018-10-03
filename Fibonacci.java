863fc9753d00882d88217e7e416cc03cb4d810a6
Haoqi Zhang <haoqi_zhang@m013479.aw.org>
2018-10-03 13:50:53 -0700

    Fibonacci

---
+++ Fibonacci.java
@@ -0,0 +1,25 @@
+//Maggie Zhang
+//Fibonacci.java
+
+
+import java.util.*;
+
+public class Fibonacci{
+  public static int Fib(int n) {
+    if(n == 1){
+      return 1;
+    }
+    else if(n == 2){
+      return 1;
+    }
+    else{
+      return Fib(n-1) + Fib(n-2);
+    }
+  }
+  public static void main(String[] args) {
+    int num = 7;
+    int tot = Fib(num);
+    System.out.println(tot);
+
+  }
+}

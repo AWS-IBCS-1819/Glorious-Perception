068323c428722101d5e34b432e8f0037433adf4f
Haoqi Zhang <haoqi_zhang@m013479.aw.org>
2018-10-09 13:46:19 -0700

    Deck

---
+++ Deck.java
@@ -0,0 +1,14 @@
+/* Deck.java by Maggie Z
+This file contains all of the data used
+to create a deck of cards */
+import java.util.*;
+
+public class Deck {
+
+  ArrayList<String> cards;
+
+  public Deck() {
+    cards = new ArrayList<String>();
+    cards.add("this deck");
+  }
+}

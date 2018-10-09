6250e6dd7ea5cb5d791c87b08cf2819ca4c5f999
Haoqi Zhang <haoqi_zhang@m013479.aw.org>
2018-10-09 13:46:47 -0700

    Pokerhand

---
+++ Pokerhand.java
@@ -0,0 +1,15 @@
+/* Pokerhand.java by Maggie Z
+This class holds all of the data and methods for
+generating poker hands using arrays. */
+
+public class Pokerhand {
+
+  public static void main(String[] args) {
+
+    Deck d = new Deck();
+
+    System.out.println(d.cards);
+
+  }
+
+}

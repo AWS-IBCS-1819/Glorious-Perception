9bc013b45b4c5d65933a42a7041eb05a93057d11
Haoqi Zhang <haoqi_zhang@m013479.aw.org>
2018-10-31 13:48:55 -0700

    Halloweeeen

---
+++ Spook.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Spook{
+  public static void main(String[] args) {
+    Map< String, Integer > m = new HashMap<String, Integer>();
+    m.put("Ursula", 5);
+    m.put("Cruella De Wil", 6);
+    m.put("The Witch", 7);
+    m.put("Jafar", 4);
+    m.put("Queen of Hearts", 3);
+    m.put("Clown", 8);
+
+    System.out.println(m);
+  }
+}
